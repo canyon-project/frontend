9c2c75d79d6b551c2703dd5709147d6654132cfd
wr.zhang25 <wr.zhang25@gmail.com>
2025-06-10 23:45:12 +0800

    chore: daily development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import Pages from 'vite-plugin-pages';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+const ReactCompilerConfig = {
+  target: '19', // '17' | '18' | '19'
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+    Pages({
+      dirs: 'src/routes',
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      // 'canyon-report': path.resolve(__dirname, '../report/src/index.tsx'),
+    },
+  },
+  build: {
+    sourcemap: true,
+    // target: 'ES2022',
+  },
+  server: {
+    port: 8000,
+    host: '0.0.0.0',
+    proxy: {
+      '^/graphql|^/api': {
+        changeOrigin: true,
+        target: 'http://localhost:8080',
+      },
+    },
+  },
+});
